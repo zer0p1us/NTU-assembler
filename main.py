6c9bce0f275444eeccbcfe952f090559a927c13a
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-03-02 23:23:49 +0000

    instructions moved to ISA.csv file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 DEBUG = True
 
 def to_hex(val, nbits = 12):
@@ -11,39 +13,34 @@ def debug_output(machine_code):
     if DEBUG:
         print(str(len(machine_code)-1) + " : " + machine_code[len(machine_code)-1])
 
-def generate_machine_code(assembly, machine_code):
+def read_ISA():
+    ISA = {}
+    with open("./ISA.csv", 'r', newline='') as ISA_file:
+        reader = csv.DictReader(ISA_file)
+        for row in reader:
+            # print(row)
+            ISA[row['instruction']] = [row['opcode'], row['operand_bit_size']]
+        ISA_file.close()
+    return ISA
+
+
+def generate_machine_code(assembly, machine_code, ISA):
     for line in assembly:
         if line.isspace():
             continue
 
-        match line.split(' ', 1)[0]:
-            case "MOVEI":
-                machine_code.append("C"+ to_hex(int(line.split(' ')[1])))
-
-                debug_output(machine_code)
-
-            case "JMP":
-                machine_code.append("E" + to_hex(int(line.split(' ')[1])))
-
-                debug_output(machine_code)
-
-            case "ADDI":
-                machine_code.append("D" + to_hex(int(line.split(' ')[1])))
-
-                debug_output(machine_code)
-
-            case "BZ":
-                machine_code.append("FE" + to_hex(int(line.split(' ')[1]), nbits=8))
-
-                debug_output(machine_code)
-
-
-            case _:
-                print(line.partition(' ')[0] + " machine_code is instruction has not been implemented")
-                machine_code.append(line)
+        if (line.split(' ', 1)[0] not in ISA):
+            print(line.partition(' ')[0] + " machine_code is instruction has not been implemented")
+            machine_code.append(line)
+        else:
+            opcode = ISA[line.split(' ', 1)[0]][0]
+            operand_bit_size = ISA[line.split(' ', 1)[0]][1]
+            machine_code.append(opcode + to_hex(int(line.split(' ')[1]), int(operand_bit_size)))
+            debug_output(machine_code)
 
 
 def main():
+    ISA = read_ISA()
     file_name = input("Please enter the filename of the assembly source: ")
     try:
         assembly_file = open(file_name, 'r')
@@ -54,11 +51,12 @@ def main():
         exit()
 
     machine_code = []
-    generate_machine_code(assembly, machine_code)
+    generate_machine_code(assembly, machine_code, ISA)
 
-    machine_code_output = open('machine_code.cdm', 'w')
+    machine_code_file = open('machine_code.cdm', 'w')
     for (instruction_mem, instruction) in enumerate(machine_code):
-        machine_code_output.writelines(str(instruction_mem) + ' : ' + instruction + '\n')
+        machine_code_file.writelines(str(instruction_mem) + ' : ' + instruction + '\n')
+    machine_code_file.close()
 
 
 if __name__ == "__main__":
