910fdfc7f56a9171074c49dbc8a89a9c5057a316
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2022-05-01 23:57:00 +0100

    moved main() above other functions defenitions

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,48 @@ void debug_output(str_vector *machine_code);
 void generate_machine_code(str_vector *assembly, str_vector *machine_code, dictionary *ISA);
 
 
+int main(int argc, char const *argv[]) {
+    // will be read from a CSV file eventually
+    dictionary ISA = {{"JMP","E"},
+                                        {"ADDI","D"},
+                                        {"MOVEI","C"},
+                                        {"MOVE","B"},
+                                        {"STORE","A"},
+                                        {"LDA","9"},
+                                        {"LDSP","8"},
+                                        {"BZ","FE"},
+                                        {"BNZ","FD"},
+                                        {"STA","FFFF"},
+                                        {"STA+","FFFE"},
+                                        {"MOVE(A)","FFFD"},
+                                        {"MOVE(A)+","FFFC"},};
+    std::string file_name;
+    std::cout << "Please enter the filename of the assembly source: ";
+    std::cin >> file_name;
+
+    str_vector assembly;
+
+    try {
+        std::fstream assembly_file(file_name);
+        std::string line;
+        while (assembly_file.good()) {
+            std::getline(assembly_file, line);
+            if (line.empty()) continue;
+            assembly.push_back(line);
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return -1;
+    }
+
+    str_vector machine_code;
+    generate_machine_code(&assembly, &machine_code, &ISA);
+
+
+    return 0;
+}
 std::string to_hex(int val, int hexBits){
     int nbits = hexBits * 4;
     std::stringstream stream;
@@ -68,47 +110,4 @@ void generate_machine_code(str_vector *assembly, str_vector *machine_code, dicti
 
         }
     }
-}
-
-int main(int argc, char const *argv[]) {
-    // will be read from a CSV file eventually
-    std::map<std::string, std::string> ISA = {{"JMP","E"},
-                                        {"ADDI","D"},
-                                        {"MOVEI","C"},
-                                        {"MOVE","B"},
-                                        {"STORE","A"},
-                                        {"LDA","9"},
-                                        {"LDSP","8"},
-                                        {"BZ","FE"},
-                                        {"BNZ","FD"},
-                                        {"STA","FFFF"},
-                                        {"STA+","FFFE"},
-                                        {"MOVE(A)","FFFD"},
-                                        {"MOVE(A)+","FFFC"},};
-    std::string file_name;
-    std::cout << "Please enter the filename of the assembly source: ";
-    std::cin >> file_name;
-
-    std::vector<std::string> assembly;
-
-    try {
-        std::fstream assembly_file(file_name);
-        std::string line;
-        while (assembly_file.good()) {
-            std::getline(assembly_file, line);
-            if (line.empty()) continue;
-            assembly.push_back(line);
-        }
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-        return -1;
-    }
-
-    std::vector<std::string> machine_code;
-    generate_machine_code(&assembly, &machine_code, &ISA);
-
-
-    return 0;
 }
